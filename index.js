4ceb6c2d861309f6117e0fdff53b8d63e8c5931d
Roland <rolandfbk@gmail.com>
2022-04-19 17:50:27 +0200

    Class implementation done

--- index.js
+++ index.js
@@ -3,33 +3,48 @@ const bookAuthor = document.getElementById('bookAuthor');
 const bookForm = document.getElementById('bookForm');
 const bookRec = document.getElementById('books-record');
 
+const booksrecord = JSON.parse(localStorage.getItem('bookArchive')) || [];
+
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
     this.id = new Date().getTime().toString().concat(performance.now());
   }
-}
 
-const booksrecord = JSON.parse(localStorage.getItem('bookArchive')) || [];
+  displayBook(title, author, id) {
+    const templateHTML = `
+      <tr>
+        <td class="table-item">
+          "${title}" by ${author}
+          <button type='button' class="remove-bttn" id="${id}">Remove</button>
+        </td>
+      </tr>`;
 
-function displayBook(title, author, id) {
-  const templateHTML = `
-    <tr>
-      <td class="table-item">
-        "${title}" by ${author}
-        <button type='button' class="remove-bttn" id="${id}">Remove</button>
-      </td>
-    </tr>`;
+    bookRec.insertAdjacentHTML('beforeend', templateHTML);
+  }
 
-  bookRec.insertAdjacentHTML('beforeend', templateHTML);
-}
+  getBooks() {
+    const storage = JSON.parse(localStorage.getItem('bookArchive'));
+    storage.forEach((book) => {
+      this.displayBook(book.title, book.author, book.id);
+    });
+  }
 
-function getBooks() {
-  const storage = JSON.parse(localStorage.getItem('bookArchive'));
-  storage.forEach((book) => {
-    displayBook(book.title, book.author, book.id);
-  });
+  remove(button, key) {
+    button.addEventListener('click', () => {
+      if (key === 0) {
+        booksrecord.splice(key, key + 1);
+      } else {
+        booksrecord.splice(key, key);
+      }
+
+      bookRec.innerHTML = '';
+      localStorage.setItem('bookArchive', JSON.stringify(booksrecord));
+      window.location.reload();
+      localStorage.setItem('bookStorage', JSON.stringify(booksrecord));
+    });
+  }
 }
 
 function insertBook() {
@@ -41,27 +56,18 @@ function insertBook() {
     bookTitle.focus();
     bookAuthor.focus();
     localStorage.setItem('bookArchive', JSON.stringify(booksrecord));
-    getBooks();
+    inputBook.getBooks();
   });
 
+  const showBooks = new Book();
   localStorage.setItem('bookArchive', JSON.stringify(booksrecord));
-  getBooks();
+  showBooks.getBooks();
 }
 
 insertBook();
 
 const removeBtn = document.querySelectorAll('.remove-bttn');
 removeBtn.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    if (index === 0) {
-      booksrecord.splice(index, index + 1);
-    } else {
-      booksrecord.splice(index, index);
-    }
-
-    bookRec.innerHTML = '';
-    localStorage.setItem('bookArchive', JSON.stringify(booksrecord));
-    window.location.reload();
-    localStorage.setItem('bookStorage', JSON.stringify(booksrecord));
-  });
+  const removeBtn = new Book();
+  removeBtn.remove(btn, index);
 });
